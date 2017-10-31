591709bce214adb4bbf2a046b129190f9391a34a
Tamási Benjamin <h@lfto.me>
2017-10-31 10:45:16 +0100

    working

--- gpio.py
+++ gpio.py
@@ -2,10 +2,12 @@ import RPi.GPIO as rio
 
 
 class Port:
-    def __init__(self, number, mode='in'):
+    def __init__(self, number, mode='in', default=1):
         self.number = number
         self._mode = None
+        self._default = None
         self.mode = mode
+        self.default = default
 
     @property
     def mode(self):
@@ -19,7 +21,10 @@ class Port:
         if value == 'in':
             if self.mode == 'in':
                 rio.remove_event_detect(self.number)
-            rio.setup(self.number, rio.IN, pull_up_down=rio.PUD_UP)
+            if self.default == 1:
+                rio.setup(self.number, rio.IN, pull_up_down=rio.PUD_UP)
+            else:
+                rio.setup(self.number, rio.IN, pull_up_down=rio.PUD_DOWN)
             self._mode = 'in'
             rio.add_event_detect(self.number, rio.BOTH, self._onChange, bouncetime=50)
         elif value == 'out':
@@ -27,17 +32,24 @@ class Port:
                 rio.remove_event_detect(self.number)
             rio.setup(self.number, rio.OUT)
             self._mode = 'out'
+            self.level = 1
+
+    @property
+    def default(self):
+        return self._default
+
+    @default.setter
+    def default(self, value):
+        self._default = value
+        self.mode = self._mode
 
     @property
     def level(self):
-        return rio.input(self.number)
+        return 1 if rio.input(self.number) == rio.HIGH else 0
 
     @level.setter
     def level(self, value):
-        if value == 0:
-            rio.output(self.number, rio.LOW)
-        elif value == 1:
-            rio.output(self.number, rio.HIGH)
+        rio.output(self.number, rio.HIGH) if value == 1 else rio.output(self.number, rio.LOW)
 
 
 class Io:
